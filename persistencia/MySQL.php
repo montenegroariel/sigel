27ad17a9d431b76bbe172accc49dbf960720b867
Ariel Montenegro <ramontenegro@ANGEL94.HACFSA.ORG.AR>
2014-08-14 02:03:04 -0300

    mysql

---
+++ persistencia/MySQL.php
@@ -0,0 +1,67 @@
+<?php
+require_once 'configuracion.php';
+require_once PER . DIRECTORY_SEPARATOR . 'ManejadorBaseDeDatosInterface.php';
+
+class MySQL implements ManejadorBaseDeDatosInterface
+{
+    const USUARIO = 'root';
+    const CLAVE = 'piratas2012';
+    const BASE = 'sge';
+    const SERVIDOR = 'localhost';
+    private $_conexion;
+
+
+    public function conectar()
+    {
+        $this->_conexion = new PDO('mysql:
+            host='. self::SERVIDOR .
+            ';dbname='. self::BASE,
+            self::USUARIO,
+            self::CLAVE
+        );
+    }
+
+    public function desconectar()
+    {
+        $this->_conexion = null;
+    }
+    
+    public function consultar(SQL $sql)
+    {
+        $todo = array();
+        $resultado = $this->_conexion->query($sql->consultar());
+        //var_dump($resultado);
+        while ($fila = $resultado->fetch()){
+            $todo[] = $fila;
+        }
+        return $todo;
+    }
+   
+    
+    public function guardar(SQL $sql) {
+        mysqli_query($this->_conexion, $sql->guardar());
+        $guardar = $this->_conexion->insert_id;
+        return $guardar;
+    }
+    
+    public function modificar(SQL $sql) {
+        $modificar = mysqli_query($this->_conexion,$sql->modificar());
+        return $modificar;
+    }
+    
+    public function eliminar(SQL $sql) {
+        $eliminar = mysqli_query($this->_conexion, $sql->eliminar());
+        return $eliminar;
+    } 
+    
+
+
+    public function autenticarUsuario(SQL $sql)
+    {
+        $autenticar = $this->_conexion->query($sql->consultar());   
+        $resultado = $autenticar->fetch();
+        return $resultado;
+    }
+
+
+}
